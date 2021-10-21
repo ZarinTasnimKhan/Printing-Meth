af91de56c9de2858ef34312ef314ceb06e3bb621
ZarinTasnimKhan <zarintm1221@gmail.com>
2021-10-21 13:56:47 +0800

    first commit

---
+++ src/oop/Main.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int id = 178;
+	 String name = "Java";
+	 double ratio = 13.569;
+
+	 System.out.println();
+	 System.out.println("Name: "+name);
+	 System.out.println("Id: "+id+", Ratio: "+ratio);
+
+	  System.out.printf("Name: %s\n",name);
+	  System.out.printf("Id: %d, Ratio: %.2f",id,ratio);
+    }
+}
+
